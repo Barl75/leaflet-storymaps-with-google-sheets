5bbb803bda4a25ed2a6ff9f71440a05fcb82225a
Barl75 <62744694+Barl75@users.noreply.github.com>
2020-04-22 18:06:12 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1MDvzOq88M4cHP8C9xX0ei6-NanW57ceZtxOLq6Rp9tI/edit#gid=0';
